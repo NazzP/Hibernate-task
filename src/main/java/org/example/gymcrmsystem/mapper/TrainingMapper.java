55a08ecdf11701c54f0883e3220a447fbb68fd42
Nazz <nazz.panasiuk@gmail.com>
2025-01-13 04:50:16 +0200

    Mappers impl

---
+++ src/main/java/org/example/gymcrmsystem/mapper/TrainingMapper.java
@@ -0,0 +1,11 @@
+package org.example.gymcrmsystem.mapper;
+
+import org.example.gymcrmsystem.dto.TrainingDto;
+import org.example.gymcrmsystem.entity.Training;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring", uses = {TraineeMapper.class, TrainerMapper.class, TrainingTypeMapper.class})
+public interface TrainingMapper {
+    TrainingDto convertToDto(Training training);
+    Training convertToEntity(TrainingDto trainingDto);
+}
