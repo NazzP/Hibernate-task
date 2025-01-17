1c08f2214fe93b70e7979bbaaf9dbb8cee7de7cc
Nazz <nazz.panasiuk@gmail.com>
2025-01-17 16:19:30 +0200

    tests for TrainingType and User repositories

---
+++ src/test/java/org/example/gymcrmsystem/repository/TrainingTypeRepositoryTest.java
@@ -0,0 +1,44 @@
+package org.example.gymcrmsystem.repository;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.example.gymcrmsystem.config.AppConfig;
+import org.example.gymcrmsystem.entity.*;
+import org.junit.jupiter.api.*;
+import org.springframework.transaction.annotation.Transactional;
+
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Transactional
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = AppConfig.class)
+class TrainingTypeRepositoryTest {
+
+    @Autowired
+    private TrainingTypeRepository trainingTypeRepository;
+
+    @Test
+    void findByNameSuccess() {
+        Optional<TrainingType> foundTrainingType = trainingTypeRepository.findByName("Yoga");
+        assertTrue(foundTrainingType.isPresent());
+        assertEquals("Yoga", foundTrainingType.get().getTrainingTypeName());
+    }
+
+    @Test
+    void findByNameNoResult() {
+        Optional<TrainingType> foundTrainingType = trainingTypeRepository.findByName("NonExistentType");
+        assertFalse(foundTrainingType.isPresent());
+    }
+
+    @Test
+    void findByNameNull() {
+        Optional<TrainingType> foundTrainingType = trainingTypeRepository.findByName(null);
+        assertFalse(foundTrainingType.isPresent());
+    }
+}
