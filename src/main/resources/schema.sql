5d146b8fbc77bbd3966c68016d6c1397d39b396b
Nazz <nazz.panasiuk@gmail.com>
2025-01-20 02:17:53 +0200

    sql upd according to changes { PK id in entity }

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,12 @@
 CREATE TABLE IF NOT EXISTS training_types
 (
-    training_type_id   BIGINT PRIMARY KEY,
-    training_type_name VARCHAR(255) NOT NULL UNIQUE,
-    CONSTRAINT check_training_type_name CHECK (training_type_name IN
-                                               ('Yoga', 'Cardio', 'Strength Training', 'CrossFit'))
+    id   BIGINT PRIMARY KEY,
+    training_type_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS users
 (
-    user_id    BIGINT PRIMARY KEY,
+    id    BIGINT PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name  VARCHAR(255) NOT NULL,
     username   VARCHAR(255) NOT NULL UNIQUE,
@@ -18,34 +16,34 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS trainees
 (
-    trainee_id    BIGINT PRIMARY KEY,
+    id    BIGINT PRIMARY KEY,
     date_of_birth DATE,
     address       VARCHAR(255),
     user_id       BIGINT NOT NULL UNIQUE,
-    CONSTRAINT fk_trainee_user FOREIGN KEY (user_id) REFERENCES users (user_id)
+    CONSTRAINT fk_trainee_user FOREIGN KEY (user_id) REFERENCES users (id)
 );
 
 CREATE TABLE IF NOT EXISTS trainers
 (
-    trainer_id     BIGINT PRIMARY KEY,
+    id     BIGINT PRIMARY KEY,
     user_id        BIGINT NOT NULL UNIQUE,
     specialization BIGINT NOT NULL,
-    CONSTRAINT fk_trainer_user FOREIGN KEY (user_id) REFERENCES users (user_id),
-    CONSTRAINT fk_trainer_specialization FOREIGN KEY (specialization) REFERENCES training_types (training_type_id)
+    CONSTRAINT fk_trainer_user FOREIGN KEY (user_id) REFERENCES users (id),
+    CONSTRAINT fk_trainer_specialization FOREIGN KEY (specialization) REFERENCES training_types (id)
 );
 
 CREATE TABLE IF NOT EXISTS trainings
 (
-    training_id      BIGINT PRIMARY KEY,
+    id      BIGINT PRIMARY KEY,
     trainee_id       BIGINT,
     trainer_id       BIGINT,
     training_name    VARCHAR(255) NOT NULL,
     training_type_id BIGINT,
     date             DATE         NOT NULL,
     duration         INTEGER      NOT NULL,
-    CONSTRAINT fk_training_trainee FOREIGN KEY (trainee_id) REFERENCES trainees (trainee_id),
-    CONSTRAINT fk_training_trainer FOREIGN KEY (trainer_id) REFERENCES trainers (trainer_id),
-    CONSTRAINT fk_training_type FOREIGN KEY (training_type_id) REFERENCES training_types (training_type_id)
+    CONSTRAINT fk_training_trainee FOREIGN KEY (trainee_id) REFERENCES trainees (id),
+    CONSTRAINT fk_training_trainer FOREIGN KEY (trainer_id) REFERENCES trainers (id),
+    CONSTRAINT fk_training_type FOREIGN KEY (training_type_id) REFERENCES training_types (id)
 );
 
 CREATE TABLE IF NOT EXISTS trainee_trainer
@@ -53,6 +51,6 @@ CREATE TABLE IF NOT EXISTS trainee_trainer
     trainee_id BIGINT NOT NULL,
     trainer_id BIGINT NOT NULL,
     PRIMARY KEY (trainee_id, trainer_id),
-    CONSTRAINT fk_trainee_trainer_trainee FOREIGN KEY (trainee_id) REFERENCES trainees (trainee_id),
-    CONSTRAINT fk_trainee_trainer_trainer FOREIGN KEY (trainer_id) REFERENCES trainers (trainer_id)
+    CONSTRAINT fk_trainee_trainer_trainee FOREIGN KEY (trainee_id) REFERENCES trainees (id),
+    CONSTRAINT fk_trainee_trainer_trainer FOREIGN KEY (trainer_id) REFERENCES trainers (id)
 );
